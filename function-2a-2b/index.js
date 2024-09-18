56a9900aaea75e7427e058a25bbff07e25134afc
julievictoria98 <jura0002@stud.kea.dk>
2024-09-18 12:43:23 +0200

    lavet 2 og momsudregner

---
+++ function-2a-2b/index.js
@@ -0,0 +1,29 @@
+
+/* excerise 1 ----------------------------------------------------------------------- */
+// presentPet(firstName, animalType, animalName);
+
+// function presentPet(firstName, animalType, animalName){
+//     console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName} `)
+// }
+
+// De er ikke defined, så den giver fejl
+
+/* excerise 2 ----------------------------------------------------------------------- */
+
+presentPet(Name, animal, animalCalled);
+
+function presentPet(firstName, animalType, animalName){
+    console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName} `)
+}
+// Sigr det samme - De er ikke defined, så den giver fejl
+
+
+/* excerise 3 ----------------------------------------------------------------------- */
+
+
+
+/* excerise 4 ----------------------------------------------------------------------- */
+
+
+
+/* excerise 5 ----------------------------------------------------------------------- */
---
+++ momsudregner/index.js
@@ -0,0 +1,8 @@
+
+
+momsUdrgner(125)
+
+function momsUdrgner(beloeb, moms = 25){
+    console.log(`beløbet med moms er: ${beloeb * (1 + (moms/100))}`)
+
+}

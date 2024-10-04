ccf538915fddd9d6a9d512655f85ac0851ca4e10
julievictoria98 <jura0002@stud.kea.dk>
2024-10-04 13:07:05 +0200

    done

---
+++ name-parts/index.js
@@ -0,0 +1,12 @@
+let firstName = "Julie";
+
+let firstpart = firstName.substring(0,2).toLowerCase();
+
+let thirdLetter = firstName.substring(2,3).toUpperCase();
+
+let finalPart = firstName.substring(3, firstName.length).toLowerCase();
+let newName = firstpart + thirdLetter + finalPart;
+
+
+console.log(newName);
+

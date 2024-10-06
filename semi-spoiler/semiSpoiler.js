bb7026da33404ff9db6b2f57b71e1c866c64de3e
julievictoria98 <jura0002@stud.kea.dk>
2024-10-06 12:39:18 +0200

    lavet semiSpoiler

---
+++ semi-spoiler/semiSpoiler.js
@@ -0,0 +1,29 @@
+// prøver med .replaceAll()
+
+const theText = "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+
+const result = theText.replaceAll("ord", "ting");
+
+console.log(result);
+
+
+// prøver med spil/join
+
+const theText2 = "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+const textArray = theText2.split(" ");
+
+const updatedWord = textArray.map((object) => {
+    if(object === "ord"){
+        return "ting";
+    }
+    return object;
+}
+);
+newString = updatedWord.join(" ");
+
+console.log(newString);
+
+
+
+
+
